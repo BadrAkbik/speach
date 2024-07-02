4bf71b84f17aaab06c19291eabe7e209738b7ed7
BadrAkbik <badrakbik1@email.com>
2024-07-02 20:08:08 +0300

    assign mobile verifies middlware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\EnsureEmailIsVerified;
+use App\Http\Middleware\EnsureMobileIsVerifiedMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,10 +16,12 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
+        ])
+        ->append(EnsureMobileIsVerifiedMiddleware::class);
 
         $middleware->alias([
-            'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+            'verified' => EnsureEmailIsVerified::class,
+            'mobile.verified' => EnsureMobileIsVerifiedMiddleware::class,
         ]);
 
         //
