0e25a3a67ce372ed5aa2a3d332c06c230313779d
BadrAkbik <badrakbik1@email.com>
2024-07-04 18:42:37 +0300

    filament installation

---
+++ lang/ar/dashboard.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'dashboard' => 'لوحة التحكم',
+    'users' => 'المستخدمون',
+    'settings' => 'الإعدادات',
+    'reports' => 'التقارير',
+    'user' => 'مستخدم',
+    'roles' => 'أدوار',
+    'role' => 'دور',
+    'permissions' => 'الأذونات',
+    'permission' => 'أذن',
+];
---
+++ lang/en/dashboard.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'dashboard' => 'Dashboard',
+    'settings' => 'Settings',
+    'reports' => 'Reports',
+    'users' => 'Users',
+    'user' => 'User',
+    'roles' => 'Roles',
+    'role' => 'Role',
+    'permissions' => 'Permissions',
+    'permission' => 'Permission',
+];
